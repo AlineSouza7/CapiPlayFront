62c65ed8c3678c34ed567464837369ce64663b28
MathiasDziurkowski <mathiasdziurkowski@gmail.com>
2023-07-07 21:44:54 -0300

    iniciando página de histórico e fazendo header back

--- src/pages/historic/Historic.js
+++ src/pages/historic/Historic.js
@@ -1,6 +1,24 @@
+import "./Historic.css"
+import { useState } from "react"
+import HeaderToBack from "../../components/headerToBack/HeaderToBack"
+
 const Historic = () => {
 
-    
+    // trocar o tipo do state para {} quando houver o Object Video ou Shorts
+    const [videos, setVideos] = useState(true);
+    const [shorts, setShorts] = useState(true);
 
+    return (
+        <div className="historic__container">
+            <HeaderToBack text={"Histórico"} route={"/"} />
+            {
+                videos || shorts
+                    ?
+                    <span>Data</span>
+                    :
+                    <span>Você ainda não viu nenhum vídeo</span>
+            }
+        </div>
+    )
 }
 export default Historic
