45051658d4a5787730c2f1e795dd915563560238
erikawk <105462871+erikawk@users.noreply.github.com>
2023-07-11 19:35:50 -0300

    iniciando histórico

--- src/pages/historic/Historic.js
+++ src/pages/historic/Historic.js
@@ -1,12 +1,22 @@
 import "./Historic.css"
 import { useState } from "react"
 import HeaderToBack from "../../components/headerToBack/HeaderToBack"
+import img_video from "../../assets/image/img_video.png"
 
 const Historic = () => {
 
-    // trocar o tipo do state para {} quando houver o Object Video ou Shorts
-    const [videos, setVideos] = useState(true);
-    const [shorts, setShorts] = useState(true);
+    // state de vídeo apenas para base 
+    const [videos, setVideos] = useState({
+        uuid: 1,
+        caminho: "",
+        descricao: "Descrição do Vídeo",
+        titulo: "Título do Vídeo",
+        listaTags: [],
+        categoria: {},
+        miniatura: { img_video }
+    });
+
+    const [shorts, setShorts] = useState([]);
 
     return (
         <div className="historic__container">
@@ -14,7 +24,22 @@ const Historic = () => {
             {
                 videos || shorts
                     ?
-                    <span>Data</span>
+                    <div className="historic__date">
+                        <span>Data</span>
+                        {shorts != null &&
+                            <div className="container__shorts__historic">
+                                <div></div>
+                                <div></div>
+                            </div>
+                        }
+                        {videos &&
+                            <div className="container__videos__historic">
+                                <img src={videos.miniatura} alt="" />
+                                <div></div>
+                                <div></div>
+                            </div>
+                        }
+                    </div>
                     :
                     <span>Você ainda não viu nenhum vídeo</span>
             }
